169ecfd8b99502b382719ffcc471737af8ccce21
Ana Almendras <almendras451@gmail.com>
2025-06-29 18:54:37 -0400

    test corriendo

--- monitoreo-peso/src/test/java/com/ejemplo/TestActualizarPeso.java
+++ monitoreo-peso/src/test/java/com/ejemplo/TestActualizarPeso.java
@@ -1,15 +1,17 @@
 package test.java.com.ejemplo;
+
+import main.java.com.ejemplo.Usuario;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-import main.java.com.ejemplo.Usuario;
 
-class UsuarioTest{
-Usuario usuario = new Usuario("Maria",150);
-@Test
-void testActualizarPeso() {
-double newPeso = usuario.actualizarPeso(130);
-assertEquals(130, newPeso);
-}
+class UsuarioTest {
+    Usuario usuario = new Usuario("Maria", 150);
 
+    @Test
+    void testActualizarPeso() {
+        usuario.actualizarPeso(130);
+        double newPeso = usuario.getPeso();
+        assertEquals(130, newPeso);
+    }
 
 }
