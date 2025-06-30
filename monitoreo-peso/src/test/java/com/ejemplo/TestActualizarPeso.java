cee0e6a59f88a8c18b8b8f798221b6c864030b71
Ana Almendras <almendras451@gmail.com>
2025-06-30 18:41:24 -0400

    se creo una pueba funcional del flujo que aria un usuario para ingresar su nombre y peso

--- monitoreo-peso/src/test/java/com/ejemplo/TestActualizarPeso.java
+++ monitoreo-peso/src/test/java/com/ejemplo/TestActualizarPeso.java
@@ -1,29 +1,30 @@
 package com.ejemplo;
+
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class UsuarioTest {
     Usuario usuario = new Usuario("Maria", 150);
-        @Test
+
+    @Test
     void testGetPeso() {
 
         double newPeso = usuario.getPeso();
         assertEquals(150, newPeso);
     }
-            @Test
+
+    @Test
     void testGetNombre() {
-  
+
         String newNombre = usuario.getNombre();
         assertEquals("Maria", newNombre);
     }
+
     @Test
     void testActualizarPeso() {
         usuario.actualizarPeso(130);
         double newPeso = usuario.getPeso();
         assertEquals(130, newPeso);
     }
-
-
-
 
 }
